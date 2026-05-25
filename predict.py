bf890744df04fa7bf195812cdfcbd23605066208
maniparihar29048-creator <maniparihar29048@gmail.com>
2026-05-25 17:47:15 +0530

    Update predict.py

--- predict.py
+++ predict.py
@@ -1,29 +1,21 @@
+import pandas as pd
 import joblib
-import numpy as np
 
-# Load Model and Scaler
-model = joblib.load("model.pkl")
+model = joblib.load("heart_disease_model.pkl")
 scaler = joblib.load("scaler.pkl")
 
-# Example Input Data
-# Format:
-# [age, sex, cp, trestbps, chol,
-#  fbs, restecg, thalach, exang,
-#  oldpeak, slope, ca, thal]
-
-sample_data = np.array([
-    [52, 1, 0, 125, 212,
-     0, 1, 168, 0,
-     1.0, 2, 2, 3]
+sample_data = pd.DataFrame([[
+    52,1,0,125,212,0,1,168,0,1.0,2,2,3
+]], columns=[
+    'age','sex','cp','trestbps','chol','fbs',
+    'restecg','thalach','exang','oldpeak',
+    'slope','ca','thal'
 ])
 
-# Scale Data
 scaled_data = scaler.transform(sample_data)
 
-# Predict
 prediction = model.predict(scaled_data)
 
-# Output Result
 if prediction[0] == 1:
     print("Heart Disease Detected")
 else:
